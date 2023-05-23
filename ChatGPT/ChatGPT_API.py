e975963b4256b0faf8842c0970872957a12d82c6
peterlord22 <pd1080@usnh.edu>
2023-05-23 18:52:47 +0000

    CHATGPT

---
+++ ChatGPT/ChatGPT_API.py
@@ -0,0 +1,12 @@
+"""
+ChatGPT_API.py
+Peter Dinh
+"""
+
+import openai
+
+openai.api_key = "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"
+
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
+messages=[{"role": "user", "content": "Write an essay about Vietnam "}])
+print(completion.choices[0].message.content)
